5e97be14f559b2be9e0fd7d80c11ed25ed130d8c
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-10-19 22:00:14 +0500

    feat: add simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,74 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const rawInput = {
+      a: 7,
+      b: 5,
+      action: Action.Add,
+    };
+    const result = simpleCalculator(rawInput);
+    expect(result).toBe(12);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const rawInput = {
+      a: 7,
+      b: 5,
+      action: Action.Subtract,
+    };
+    const result = simpleCalculator(rawInput);
+    expect(result).toBe(2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const rawInput = {
+      a: 7,
+      b: 5,
+      action: Action.Multiply,
+    };
+    const result = simpleCalculator(rawInput);
+    expect(result).toBe(35);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const rawInput = {
+      a: 100,
+      b: 10,
+      action: Action.Divide,
+    };
+    const result = simpleCalculator(rawInput);
+    expect(result).toBe(10);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const rawInput = {
+      a: 2,
+      b: 3,
+      action: Action.Exponentiate,
+    };
+    const result = simpleCalculator(rawInput);
+    expect(result).toBe(8);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const rawInput = {
+      a: 100,
+      b: 10,
+      action: '/7/7/',
+    };
+    const result = simpleCalculator(rawInput);
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const rawInput = {
+      a: '100',
+      b: '10',
+      action: Action.Divide,
+    };
+    const result = simpleCalculator(rawInput);
+    expect(result).toBeNull();
   });
 });
